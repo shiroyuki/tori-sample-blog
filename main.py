b613187b6d973aa3be477eeff45d70822e83f0ba
Juti Noppornpitak <jnopporn@shiroyuki.com>
2013-04-17 22:27:36 -0400

    Got the demo app ready.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import re
 from flask import Flask
 from flask import render_template as rt
+from flask import redirect, request
 from tori.db.criteria import Criteria, Order
 from tori.db.manager import Manager
-from model import Post
+from model import Post, Tag
 
 app = Flask(__name__)
 em  = Manager('tori_sample_blog')
@@ -17,9 +19,50 @@ def home():
 
     return rt('index.html', posts=posts)
 
-@app.route('/post/', methods=['GET'])
-def post():
-    return
+@app.route('/tags/')
+def tags():
+    session = em.open_session()
+    repo    = session.repository(Tag)
+
+    criteria = Criteria(order_by={'label': Order.ASC})
+    tags     = repo.find(criteria)
+
+    return rt('tags.html', tags=tags)
+
+@app.route('/tags/<label>')
+def tag(label):
+    session = em.open_session()
+    repo    = session.repository(Tag)
+
+    criteria = Criteria({'label': label}, limit=1)
+    tag      = repo.find(criteria)
+
+    return rt('tags.get.html', tag=tag)
+
+@app.route('/post/new', methods=['GET'])
+def post_new():
+    return rt('post.new.html')
+
+@app.route('/post/', methods=['POST'])
+def post_create():
+    session = em.open_session()
+    posts   = session.repository(Post)
+    tags    = session.repository(Tag)
+
+    content  = re.sub('\r', '', request.form.get('content').strip())
+    raw_tags = re.sub('\r', '', request.form.get('tags', '').strip())
+
+    tag_entities = []
+
+    for raw_tag in re.split('\s{1,}', raw_tags):
+        tag = tags.find(Criteria({'label': raw_tag}, limit=1)) or Tag(label=raw_tag)
+        tag_entities.append(tag)
+
+    post = posts.new(content=content, tags=tag_entities)
+
+    posts.post(post)
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import re
 import time
 from tori.db.entity import entity
-from tori.db.mapper import link, AssociationType as t
+from tori.db.mapper import link, AssociationType as a, CascadingType as c
 
 @link(
     target='model.Post',
     mapped_by='posts',
     inverted_by='tags',
-    association=t.MANY_TO_MANY
+    association=a.MANY_TO_MANY
 )
 @entity('tags')
 class Tag(object):
@@ -17,9 +17,9 @@ class Tag(object):
 
 @link(
     target=Tag,
-    association=t.MANY_TO_MANY,
+    association=a.MANY_TO_MANY,
     mapped_by='tags',
-    read_only=True
+    cascading=[c.PERSIST]
 )
 @entity('posts')
 class Post(object):
@@ -29,17 +29,4 @@ class Post(object):
         self.created_at = created_at or time.time()
 
     def structure(self):
-        structure = {
-            'title': '',
-            'body': ''
-        }
-
-        content  = re.sub('<', '&lt;', re.sub('>', '&gt;', self.content))
-        segments = re.split('\s{2,}', content)
-
-        if len(segments) > 2:
-            structure['title'] = segments.pop(0)
-
-        structure['body']  = '<p>{}</p>'.format('</p><p>'.join(segments[1:]))
-        
-        return structure
+        return re.split('\n{2,}', content)
